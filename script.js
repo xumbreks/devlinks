606ace7603da2c38f1e375593e35aaf80ad9cb92
BRIAN <owner@MBP-de-Brian.home>
2023-08-30 21:46:29 +0100

    att

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
 
- html.classList.toggle('dark')
+  html.classList.toggle('dark')
+
+ // pegar a tag img
+ const img = document.querySelector("#profile img")
+
+ // substituir a imagem
+ if (html.classList.contains('dark')) {
+   // se tiver light mode, adicionar a imagem light
+   img.setAttribute("src", "./assets/eu8.png")
+ } else {
+   // set tiver sem light mode, manter a imagem normal
+   img.setAttribute("src", "./assets/eu9.png")
+ }
 }
